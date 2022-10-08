53f17e7268e5cf7b257782f59ed6273c3e73e20f
gcount85 <100mgml@gmail.com>
2022-10-09 02:15:33 +0900

    2.1 C언어 반복문 실습

---
+++ c-loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0;
+    while (i < 5)
+    {
+        printf("hello world\n");
+        i++;
+    }
+
+    // for 루프는 3가지 인자를 받음
+    for (int i = 0; i < 5; i++)
+    {
+        printf("hello world\n");
+    }
+}
